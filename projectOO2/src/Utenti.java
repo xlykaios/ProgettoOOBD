5882845043ca2660aa48d2f25eaf4ef876e4c4c3
Lykaxx <xLykaios@gmail.com>
2021-03-07 03:04:50 +0100

    Update Costruttore

--- projectOO2/src/Utenti.java
+++ projectOO2/src/Utenti.java
@@ -1,5 +1,11 @@
 
 public class Utenti {
+	int ID;
+	String Nome;
+	String Cognome ;
+	Float Salario;
+	String CF;
+	int Valutazione;
 	public Utenti(int user_ID, String user_Name, String user_Surname, String cF2) {
 		int ID = user_ID;
 		String Nome = user_Name;
